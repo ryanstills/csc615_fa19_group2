138c70a583cdae72c7aca80b38ec60b6e420b98e
dimpl3 <dimpl3@users.noreply.github.com>
2019-12-17 11:35:30 -0800

    ultrasonic sensor is now reading more reliability. added avg_distance() to calculate the last five distances the US sensors has read in order to control for fluke readings.

--- car/main.c
+++ car/main.c
@@ -10,14 +10,19 @@
 //contains defintion of wiringPi pin refs
 #include "pins.h"
 
-#define MAX_DISTANCE 220 // define the maximum measured distance
+#define MAX_DISTANCE 220// define the maximum measured distance
 #define timeOut MAX_DISTANCE*60 // calculate timeout according to the maximum measured distance
 #define NUM_THREADS 7
 
+#define ARR_SIZE 5
+
 #define BASE_SPEED 110
-#define SAFE_DISTANCE 25
+#define TURN_SPEED 5
+#define SAFE_DISTANCE 30
 #define LEFT_MOTOR 50
 #define RIGHT_MOTOR 51
+
+#define TEST_TIME 3000
 
 /* Car Modes:
     1 - Driving
@@ -50,6 +55,7 @@ typedef struct CarInfo {
 //function pulseIn: obtain pulse time of a pin
 int pulseIn(int pin, int level, int timeout);
 void startCar(struct CarInfo *);
+float avg_distance(int, float *);
 void motorStop();
 void motorTest();
 
@@ -114,10 +120,11 @@ void * getSonar(void * carInfo){ // get the measurement results of ultrasonic mo
 	    long pingTime;
 	    float readout;
 	    digitalWrite(trigPin,HIGH); //trigPin send 10us high level
-	    delayMicroseconds(10);
+	    delayMicroseconds(2500);
 	    digitalWrite(trigPin,LOW);
 	    pingTime = pulseIn(echoPin,HIGH,timeOut); //read plus time of echoPin
-	    readout = (float)pingTime * 340.0 / 2.0 / 1000.0;
+	    readout = (float)pingTime * 340.0 / 2.0 / 10000.0;
+	
 	    car->ultrasonic_readout = (readout < 1.0) ? MAX_DISTANCE : readout; // the sound speed is 340m/s, and calculate distance
     }
 }
@@ -182,37 +189,44 @@ int main(){
     carInfo->mode = 1;
     
     startCar(carInfo);
-
+     
     int count = 0;
+    int distance_flag = 0;
+    int arr_pos = 0;
     //adjusting left motor speed by 24 to compensate for speed mismatch
     int left_motor_speed = BASE_SPEED + 24;
     int right_motor_speed = BASE_SPEED;
     int current_speed = BASE_SPEED;
     
+    float distance_arr[ARR_SIZE];
+    //~ motorStop();
+    //~ return 0;
     printf("Car not started\n");
     while(carInfo->ir_readout != 1) {}
     printf("Car started\n");
     sleep(2);
     
-
+   
     
     while(carInfo->mode != 0)
     {
+	distance_arr[arr_pos] = carInfo->ultrasonic_readout;
+	arr_pos++;
+	if(arr_pos > 4){
+	    arr_pos = 0;
+	}
 	if(carInfo->mode == 1){
-	    //~ if(carInfo->ultrasonic_readout < SAFE_DISTANCE){
-		//~ printf("Car stopped due to unsafe distance of: %2f cm\n", carInfo->ultrasonic_readout);
-		//~ motorStop();
-		//~ carInfo->mode = 2;
-		//~ printf("Changing to car mode %d\n", carInfo->mode);
-	    //~ }
-	    //~ else{
+	    
+	    if(avg_distance(ARR_SIZE, distance_arr) <= SAFE_DISTANCE){	   
+		printf("Car stopped due to unsafe distance of: %2f cm\n", carInfo->ultrasonic_readout);
+		motorStop();
+		carInfo->mode = 2;
+		printf("Changing to car mode %d\n", carInfo->mode);
+	    }
 	
 	    while(carInfo->left_line_readout == 1)
-	    {
-		printf("adjusting to the right\n");
-		printf("left motor speed: %d\n", left_motor_speed);
-		printf("right motor speed: %d\n", right_motor_speed);
-		left_motor_speed = setMotorSpeed(LEFT_MOTOR, 0, HIGH, LOW);
+	    {		
+		left_motor_speed = setMotorSpeed(LEFT_MOTOR, TURN_SPEED, HIGH, LOW);
 		right_motor_speed = setMotorSpeed(RIGHT_MOTOR, right_motor_speed,HIGH, LOW);
 		usleep(2500);
 	    }
@@ -220,24 +234,21 @@ int main(){
 	    right_motor_speed = setMotorSpeed(RIGHT_MOTOR, BASE_SPEED , HIGH, LOW);
 	    while(carInfo->right_line_readout == 1)
 	    {
-		printf("adjusting to the left\n");
-		printf("left motor speed: %d\n", left_motor_speed);
-		printf("right motor speed: %d\n", right_motor_speed);
 		left_motor_speed = setMotorSpeed(LEFT_MOTOR, left_motor_speed, HIGH, LOW);
-		right_motor_speed = setMotorSpeed(RIGHT_MOTOR, 0, HIGH, LOW);
+		right_motor_speed = setMotorSpeed(RIGHT_MOTOR, TURN_SPEED, HIGH, LOW);
 		usleep(2500);
 	    }
 	    
 	    left_motor_speed = setMotorSpeed(LEFT_MOTOR, BASE_SPEED + 24, HIGH, LOW);
 	    right_motor_speed = setMotorSpeed(RIGHT_MOTOR, BASE_SPEED , HIGH, LOW);
-	    //~ }
-	    
+
 	}
 	else if(carInfo->mode == 2){
-	    left_motor_speed = setMotorSpeed(LEFT_MOTOR, left_motor_speed , LOW, HIGH);
-	    right_motor_speed = setMotorSpeed(RIGHT_MOTOR, right_motor_speed, LOW, HIGH);
+	    printf("mode 2\n");
+	    carInfo->mode = 0;
+	    motorStop();
 	}
-	if(count > 6000){
+	if(count > TEST_TIME){
 	    carInfo->mode = 0;
 	    printf("+++++++++++++++++++++++++++++++++++++++++++++\n");
 	    motorStop();
@@ -302,6 +313,13 @@ int pulseIn(int pin, int level, int timeout)
    return micros;
 }
 
+float avg_distance(int max_size, float * distance_arr){
+    float sum = 0.0;
+    for(int i = 0; i < max_size; i++){
+	sum += distance_arr[i];
+    }
+    return sum / max_size;
+}
 void motorTest(){
     
     printf("start\n");
